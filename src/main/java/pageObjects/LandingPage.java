5dd3c81dd8d9bad494440430f06564c249785431
AkaneRaw <akshayschhajalane@gmail.com>
2021-12-17 01:00:08 +0530

    first commit

---
+++ src/main/java/pageObjects/LandingPage.java
@@ -0,0 +1,58 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+//rahulonlinetutor@gmail.com
+public class LandingPage {
+
+	
+	public WebDriver driver;
+	
+	private By signin=By.cssSelector("a[href*='sign_in']");
+	private By title=By.cssSelector(".text-center>h2");
+	private By NavBar=By.cssSelector(".nav.navbar-nav.navbar-right>li>a");
+	
+	@FindBy (css="div[class*='col-sm-12'] h2")
+	private WebElement footer;
+	
+	
+	
+	
+	
+	public LandingPage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		
+		this.driver=driver;
+		
+	}
+
+
+
+
+	public LoginPage getLogin()
+	{
+		 driver.findElement(signin).click();
+		 LoginPage lp=new LoginPage(driver);
+		 return lp;
+		 
+		 
+		 
+		 
+	}
+	public WebElement getNavigationBar()
+	{
+		return driver.findElement(NavBar);
+	}
+	public WebElement getTitle()
+	{
+		return driver.findElement(title);
+	}
+	
+	public WebElement getfooter()
+	{
+		return footer;
+	}
+	
+}
